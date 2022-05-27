c16fd5dc2431fc24aeb2bceb826677eea64fcfc9
Arnascode <vilkoncius7@gmail.com>
2022-05-27 15:01:10 +0300

    login register html done

---
+++ public/js/register.js
@@ -0,0 +1,59 @@
+console.log('register here');
+const BASE_URL = 'http://localhost:3000';
+
+const formEl = document.forms.register;
+const errroEl = document.getElementById('err');
+
+function handleError(msg) {
+  errroEl.textContent = '';
+  if (typeof msg === 'string') {
+    errroEl.textContent = msg;
+  }
+  if (Array.isArray(msg)) {
+    msg.forEach((eObj) => {
+      errroEl.innerHTML += `${eObj.message}<br>`;
+    });
+  }
+}
+
+async function registerFetch(fullName, email, password) {
+  const registerObj = { fullName, email, password };
+  const resp = await fetch(`${BASE_URL}/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(registerObj),
+  });
+  if (resp.status === 201) {
+    // success
+    handleError('register success');
+    window.location.replace('login.html');
+  } else {
+    // fail
+    handleError(await resp.json());
+  }
+}
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('reg');
+  // 1. paimti formos duomenis
+  const formData = {
+    fullName: formEl.elements.fullName.value,
+    email: formEl.elements.email.value,
+    password: formEl.elements.password.value,
+    repeatPassword: formEl.elements.repeat_password.value,
+  };
+  console.log('formData ===', formData);
+  // 2. palytingi ar sutampa slaptazodziai
+  if (formData.password !== formData.repeatPassword) {
+    handleError('Password dont match');
+    return;
+  }
+
+  registerFetch(formData.fullName, formData.email, formData.password);
+});
+
+// 3. jei sutampa siusti i registracijos endpointa
+// 4. pranesti apie klaida jei tokia ivyko
